abf64596f86985266888af82835aa5e4c41e931f
once-ler <htao@eztier.com>
2018-06-01 10:49:32 -0400

    Temporarily disable yellowbox warnings

--- example/index.android.js
+++ example/index.android.js
@@ -1,2 +1,9 @@
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+])
+
 import App from './src/modules/App/App'
 const app = new App()
--- example/index.ios.js
+++ example/index.ios.js
@@ -1,3 +1,10 @@
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+])
+
 import { AppRegistry } from 'react-native'
 import App from './src/modules/App/App'
 console.log('ios')
